7ee3fc7c22d06ae8fb879c8177a3df0bcad1c558
akash <akigupta131@gmail.com>
2015-03-30 23:15:49 +0530

    revised boundary conditions for neumann and dirichlet

--- pdex4bc.m
+++ pdex4bc.m
@@ -1,5 +1,17 @@
-function [pl,ql,pr,qr] = pdex4bc(xl,ul,xr,ur,t)
-pl = [0; ul(2)]; 
-ql = [1; 0];     
-pr = [ur(1)-1; 0]; 
-qr = [0; 1];     
+function [pl,ql,pr,qr] = pdex4bc(rl,ul,rr,ur,t)
+ 
+% Constants
+A = 2;
+B = 1; %NOT GIVEN | TO BE CHANGED
+ 
+% Case 1:- Zero Flux Boundary Conditions (Neumann conditions)
+% pl = [0; 0];
+% ql = [1; 1];
+% pr = [0; 0];
+% qr = [1; 1];
+ 
+% Case 2:- Fixed Boundary Conditions (Dirichlet conditions)
+pl = [A; B/A];
+ql = [0; 0];
+pr = [A; B/A];
+qr = [0; 0];
