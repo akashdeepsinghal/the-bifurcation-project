a4bc8deaa42bf72ea733b16d1b8647e764b46170
akash <akigupta131@gmail.com>
2015-03-31 03:04:04 +0530

    Cleared out the junk | corrected diffuion coeff and constants' values

--- pdex4ic.m
+++ pdex4ic.m
@@ -4,10 +4,3 @@ c2 = 10;
 L = 1;
 
 u0 = [c1*cos(pi*r/L); c2*cos(pi*r/L)];
-
-
-% a=0.92;
-% p=2;
-% u_star=a^(1-p);
-% v_star=a;
-% u0=[u_star+0.6068*cos(10*r);v_star+0.6068*cos(10*r)];
--- pdex4pde.m
+++ pdex4pde.m
@@ -1,14 +1,8 @@
 function [c,f,s] = pdex4pde(r,t,u,DuDr)
 
-disp (DuDr);
 % Diffusion Coefficients
-% Dy = 1.6*10^(-3);
-% Dx = 8.0*10^(-3);
-
-
-Dy = 3;
-Dx = 1;
-
+Dy = 1.6*10^(-3);
+Dx = 8.0*10^(-3);
 
 % Constants
 A = 2;
@@ -21,17 +15,12 @@ B = 3.7; %NOT GIVEN | TO BE CHANGED
 c = [1; 1];
 f = [Dx; Dy] .* DuDr;
 
-% s1 = A + u1^2*u2 - (B+1)*u1;
-% s2 = B*u1 - u1^2*u2;
-s1 = (B-1)*u1 + A^2*u2;
-s2 = -B*u1 - A^2*u2;
+% Rate equations describing the phenomenon
+s1 = A + u1^2*u2 - (B+1)*u1;
+s2 = B*u1 - u1^2*u2;
+
+% Linearized equations for the perturbation x and y
+% s1 = (B-1)*u1 + A^2*u2;
+% s2 = -B*u1 - A^2*u2;
+
 s = [s1; s2];
-
-
-% a=0.92;
-% p=2;
-% d1=1;
-% d2=3;
-% c=[1;1];
-% f=[d1;d2].*DuDr;
-% s=[a-u(1)*u(2)^p;u(1)*u(2)^p-u(2)];
